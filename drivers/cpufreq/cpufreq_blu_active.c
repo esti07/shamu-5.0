c0e7258bd1b7b19ae27285518fe64e1eff861025
engstk <eng.stk@sapo.pt>
2015-03-16 13:02:51 +0000

    blu_active: Only wake CPU input boost on multitouch events
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/cpufreq/cpufreq_blu_active.c
+++ drivers/cpufreq/cpufreq_blu_active.c
@@ -967,7 +967,8 @@ static const struct input_device_id interactive_ids[] = {
 		.evbit = { BIT_MASK(EV_ABS) },
 		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
 			    BIT_MASK(ABS_MT_POSITION_X) |
-			    BIT_MASK(ABS_MT_POSITION_Y) },
+			    BIT_MASK(ABS_MT_POSITION_Y) |
+				BIT_MASK(ABS_MT_TRACKING_ID) },
 	}, /* multi-touch touchscreen */
 	{
 		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
